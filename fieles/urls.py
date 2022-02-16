a1812f9f5d2dfdd1b9496356d66e78b02285ee31
juanjesuspy <juanjesuspy@gmail.com>
2022-02-15 23:06:12 -0300

    fixed urls names

--- fieles/urls.py
+++ fieles/urls.py
@@ -6,9 +6,9 @@ from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    path('', login_required(views.DataList.as_view()), name='fieles_list'),
-    path('view/<int:pk>', login_required(views.DataDetail.as_view()), name='fieles_view'),
-    path('new', login_required(views.DataCreate.as_view()), name='fieles_new'),
-    path('edit/<int:pk>', login_required(views.DataUpdate.as_view()), name='fieles_edit'),
-    path('delete/<int:pk>', login_required(views.DataDelete.as_view()), name='fieles_delete'),
+    path('', login_required(views.DataList.as_view()), name='data_list'),
+    path('view/<int:pk>', login_required(views.DataDetail.as_view()), name='data_view'),
+    path('new', login_required(views.DataCreate.as_view()), name='data_new'),
+    path('edit/<int:pk>', login_required(views.DataUpdate.as_view()), name='data_edit'),
+    path('delete/<int:pk>', login_required(views.DataDelete.as_view()), name='data_delete'),
 ]
